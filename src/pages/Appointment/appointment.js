d0fa8608cf7ea436bcf8cbf7307f02ec77fa5195
utahisnotastate <utahisnotastate@gmail.com>
2022-05-25 02:23:40 -0400

    created steps boilerplate

--- src/pages/Appointment/appointment.js
+++ src/pages/Appointment/appointment.js
@@ -4,89 +4,134 @@ import Button from '@mui/material/Button'
 import Stepper from '@mui/material/Stepper'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
+import Paper from '@mui/material/Paper'
+import Card from '../../components/ui/Card/Card'
+import CardHeader from '../../components/ui/Card/CardHeader'
+import CardFooter from '../../components/ui/Card/CardFooter'
 import { FormikWizard } from 'formik-wizard-form'
 import Complaints from './inprogress/complaints/complaints'
 import Assessment from './inprogress/assessment/assessment'
+import PhysicalExam from './inprogress/physicalexam/physicalexam'
+import ReviewOfSystems from './inprogress/reviewofsystems/reviewofsystems'
+import Summary from './inprogress/summary/summary'
+import Finalize from './inprogress/finalize/finalize'
+import FollowUp from './inprogress/followup/followup'
+import CardBody from '../../components/ui/Card/CardBody'
 
-export default function App() {
+export default function Appointment() {
 	const [finalValues, setFinalValues] = React.useState({})
 	const [finished, setFinished] = React.useState(false)
 	return (
 		<div className="App">
-			<FormikWizard
-				activeStepIndex={0}
-				initialValues={{
-					firstName: '',
-					lastName: '',
-					username: '',
-					password: '',
-					email: '',
-					phone: '',
-					addressLine1: '',
-					addressLine2: '',
-					employerName: '',
-					designation: '',
-					totalExperience: '',
-					city: '',
-				}}
-				onSubmit={(values) => {
-					setFinalValues(values)
-					setFinished(true)
-				}}
-				steps={[
-					{
-						component: Complaints,
-					},
-					{
-						component: Assessment,
-					},
-				]}
-				validateOnNext
-			>
-				{({
-					currentStepIndex,
-					renderComponent,
-					handlePrev,
-					handleNext,
-					isNextDisabled,
-					isPrevDisabled,
-				}) => (
-					<>
-						<Box sx={{ width: '100%', my: '1rem' }}>
-							<Stepper activeStep={currentStepIndex}>
-								<Step completed={currentStepIndex > 0}>
-									<StepLabel>Complaints</StepLabel>
-								</Step>
-								<Step completed={currentStepIndex > 1}>
-									<StepLabel>Assessments</StepLabel>
-								</Step>
-							</Stepper>
-						</Box>
-						<Box my="2rem">{renderComponent()}</Box>
-						<Box display="flex" justifyContent="space-between">
-							<Button
-								disabled={isPrevDisabled}
-								onClick={handlePrev}
-								type="primary"
-								variant="contained"
+			<Card>
+				<FormikWizard
+					activeStepIndex={0}
+					initialValues={{
+						firstName: '',
+						lastName: '',
+						username: '',
+						password: '',
+						email: '',
+						phone: '',
+						addressLine1: '',
+						addressLine2: '',
+						employerName: '',
+						designation: '',
+						totalExperience: '',
+						city: '',
+					}}
+					onSubmit={(values) => {
+						setFinalValues(values)
+						setFinished(true)
+					}}
+					steps={[
+						{
+							component: Complaints,
+						},
+						{
+							component: PhysicalExam,
+						},
+						{
+							component: ReviewOfSystems,
+						},
+						{
+							component: Assessment,
+						},
+						{
+							component: FollowUp,
+						},
+						{
+							component: Summary,
+						},
+						{
+							component: Finalize,
+						},
+					]}
+					validateOnNext
+				>
+					{({
+						currentStepIndex,
+						renderComponent,
+						handlePrev,
+						handleNext,
+						isNextDisabled,
+						isPrevDisabled,
+					}) => (
+						<>
+							<CardHeader
+								color="primary"
+								sx={{ width: '100%', my: '1rem', color: 'white' }}
 							>
-								Previous
-							</Button>
-							<Button
-								disabled={isNextDisabled}
-								onClick={handleNext}
-								type="primary"
-								variant="contained"
-							>
-								{currentStepIndex === 2 ? 'Finish' : 'Next'}
-							</Button>
-						</Box>
-						<Box>
-							<pre>{JSON.stringify(finalValues, null, 2)}</pre>
-						</Box>
-					</>
-				)}
-			</FormikWizard>
+								<Stepper activeStep={currentStepIndex}>
+									<Step completed={currentStepIndex > 0}>
+										<StepLabel sx={{ color: '#fff' }}>Complaints</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 1}>
+										<StepLabel>Physical Exam</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 2}>
+										<StepLabel>Review of Systems</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 3}>
+										<StepLabel>Assessment</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 4}>
+										<StepLabel>Follow Up</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 5}>
+										<StepLabel>Summary</StepLabel>
+									</Step>
+									<Step completed={currentStepIndex > 6}>
+										<StepLabel>Finalize</StepLabel>
+									</Step>
+								</Stepper>
+							</CardHeader>
+							<CardBody my="2rem">{renderComponent()}</CardBody>
+							<CardFooter display="flex" justifyContent="space-between">
+								<Button
+									disabled={isPrevDisabled}
+									onClick={handlePrev}
+									type="primary"
+									variant="contained"
+								>
+									Previous
+								</Button>
+								<Button
+									disabled={isNextDisabled}
+									onClick={handleNext}
+									type="primary"
+									variant="contained"
+								>
+									{currentStepIndex === 2 ? 'Finish' : 'Next'}
+								</Button>
+							</CardFooter>
+							<Box>
+								<pre>{JSON.stringify(finalValues, null, 2)}</pre>
+							</Box>
+						</>
+					)}
+				</FormikWizard>
+			</Card>
 		</div>
 	)
 }
---
+++ src/pages/Appointment/inprogress/followup/followup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Followup() {
+	return (
+		<div>
+			<p>Followup</p>
+		</div>
+	)
+}
